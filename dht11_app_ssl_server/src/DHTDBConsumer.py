9fbb49741024ae15d3dc7a88b61c5a4942662f6c
dmacmi.dm@gmail.com <dmacmi.dm@gmail.com>
2025-04-29 19:35:36 -0400

    fix handler timing, documentation

--- dht11_app_ssl_server/src/DHTDBConsumer.py
+++ dht11_app_ssl_server/src/DHTDBConsumer.py
@@ -1,20 +1,26 @@
 import os
-import threading
 import time
 import sqlite3
 
 from collections import deque
 
 DB_NAME='../data/sensor_data.db'
-MAX_BATCH_SIZE = 50
+MAX_BATCH_SIZE = 20
 FLUSH_INTERVAL = 60 # seconds
 
 class DHTDBConsumer:
-    def __init__(self, fifo):
-        self.msg_queue = fifo
-        
+    def __init__(self, fifo: deque):
+        """Takes external fifo deque filled from a producer. Contains handler for filling sqlite server
+        periodically with that.
+
+        Args:
+            fifo (collections.deque): external fifo deque filled from a producer of DHT data.
+        """
+        self.fifo = fifo
 
     def _setup_connection(self):
+        """Connect to sqlite3 with path from DB_NAME.
+        """
         db_abspath = os.path.abspath(DB_NAME)
         self.conn = sqlite3.connect(db_abspath)
         self.cursor = self.conn.cursor()
@@ -28,13 +34,16 @@ class DHTDBConsumer:
         self.conn.commit()
 
     def handler(self):
+        """After connecting with sqlite db file, consume from fifo queue and insert into sensor_data table.
+        """
         self._setup_connection()
 
         batch = []
         last_flush = time.time()
         while True:
-            if len(self.msg_queue) > 0:
-                batch.append(self.msg_queue.pop())
+            if len(self.fifo) > 0:
+                while(len(self.fifo) > 0):
+                    batch.append(self.fifo.pop())
             # time or size-based flush
             if (len(batch) >= MAX_BATCH_SIZE or time.time() - last_flush >= FLUSH_INTERVAL):
                 if batch:
@@ -46,3 +55,4 @@ class DHTDBConsumer:
                     print(f"Inserted batch of {len(batch)} records.")
                     batch.clear()
                     last_flush = time.time()
+            time.sleep(1)
